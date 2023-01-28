e70038a89263eb1b42a5172771a4da470c060a15
Kateryna Ostrynska <kateriinag@gmail.com>
2023-01-28 23:50:45 +0200

    Update Navigation.styled.jsx

--- src/components/Navigation/Navigation.styled.jsx
+++ src/components/Navigation/Navigation.styled.jsx
@@ -4,7 +4,6 @@ import { NavLink } from 'react-router-dom';
 export const Link = styled(NavLink)`
   display: inline-block;
   text-decoration: none;
-  padding: 12px;
   font-weight: 700;
   color: #2a363b;
   &.active {
