6ee0e43512f2f85e0d6d8c40682704920e7aacc7
Kateryna Ostrynska <kateriinag@gmail.com>
2023-01-28 23:49:29 +0200

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,7 +4,6 @@ import { LoginForm } from 'components/LoginForm/LoginForm';
 export default function Login() {
   return (
     <HelmetProvider>
-      {' '}
       <div>
         <Helmet>
           <title>Login</title>
