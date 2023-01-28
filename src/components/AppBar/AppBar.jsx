4d5e294f40c86fb384927b83522c14d184aabf08
Kateryna Ostrynska <kateriinag@gmail.com>
2023-01-28 23:51:11 +0200

    Update AppBar.jsx

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -2,15 +2,41 @@ import { Navigation } from '../Navigation/Navigation';
 import { UserMenu } from '../UserMenu/UserMenu';
 import { AuthNav } from '../AuthNav/AuthNav';
 import { useAuth } from 'hooks';
-import { Header } from './AppBar.styled';
+import SummarizeRoundedIcon from '@mui/icons-material/SummarizeRounded';
 
-export const AppBar = () => {
+import Container from '@mui/material/Container';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import { AppBarStyled } from './AppBar.styled';
+
+export const AppBarNavigation = () => {
   const { isLoggedIn } = useAuth();
 
   return (
-    <Header>
-      <Navigation />
-      {isLoggedIn ? <UserMenu /> : <AuthNav />}
-    </Header>
+    <AppBarStyled>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <SummarizeRoundedIcon sx={{ display: { md: 'flex' }, mr: 2 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            PHONEBOOK
+          </Typography>
+          <Navigation />
+          {isLoggedIn ? <UserMenu /> : <AuthNav />}
+        </Toolbar>
+      </Container>
+    </AppBarStyled>
   );
 };
