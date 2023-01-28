029578ab916342edda8d95a93140a1976d2764f0
Kateryna Ostrynska <kateriinag@gmail.com>
2023-01-28 23:49:13 +0200

    Update selectors.jsx

--- src/redux/contacts/selectors.jsx
+++ src/redux/contacts/selectors.jsx
@@ -1,18 +1,5 @@
-import { createSelector } from '@reduxjs/toolkit';
+export const selectContacts = state => state.contacts.items;
 
-export const selectLoading = state => state.contacts.loading;
+export const selectLoading = state => state.contacts.isLoading;
 
-export const selectFilter = state => state.contacts.filter;
-
-export const selectError = state => state.contacts.error;
-
-export const selectAllContacts = state => state.contacts.items;
-
-export const selectFilteredContacts = createSelector(
-  [selectAllContacts, selectFilter],
-  (contacts, filters) => {
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filters.toLowerCase().trim())
-    );
-  }
-);
+export const selectFilter = state => state.filter;
