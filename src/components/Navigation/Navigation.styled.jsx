4c621f561dc945bf83e3166881590ba8892630cc
Kateryna Ostrynska <kateriinag@gmail.com>
2023-01-30 01:08:35 +0200

    Update Navigation.styled.jsx

--- src/components/Navigation/Navigation.styled.jsx
+++ src/components/Navigation/Navigation.styled.jsx
@@ -2,11 +2,14 @@ import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
 
 export const Link = styled(NavLink)`
-  display: inline-block;
   text-decoration: none;
+  color: #fff7e8;
   font-weight: 700;
   color: #2a363b;
+  &:not(:last-child) {
+    margin-right: 20px;
+  }
   &.active {
-    color: #e84a5f;
+    color: #d85841;
   }
 `;
