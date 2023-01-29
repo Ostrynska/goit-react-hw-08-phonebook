d28a131e04e6f637d0d87534fc9f6e06a0ac019c
Kateryna Ostrynska <kateriinag@gmail.com>
2023-01-30 01:08:39 +0200

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,3 +1,101 @@
+// import Notiflix from 'notiflix';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { addContact } from '../../redux/contacts/operations';
+// import { selectContacts, selectLoading } from '../../redux/contacts/selectors';
+// import { nanoid } from '@reduxjs/toolkit';
+
+// import { ContactsForm } from './ContactForm.styled';
+// import Box from '@mui/material/Box';
+// import Container from '@mui/material/Container';
+// import TextField from '@mui/material/TextField';
+// import Button from '@mui/material/Button';
+// import { theme } from '../App';
+
+// export const ContactForm = () => {
+//   const dispatch = useDispatch();
+//   const contacts = useSelector(selectContacts);
+//   const isLoading = useSelector(selectLoading);
+
+//   const nameId = nanoid(4);
+//   const numberId = nanoid(4);
+
+//   const handleSubmit = e => {
+//     e.preventDefault();
+//     const contactForm = e.target;
+//     const name = contactForm.elements[0].value;
+//     const number = contactForm.elements[1].value;
+
+//     const contact = {
+//       name,
+//       number,
+//     };
+
+//     if (
+//       contacts
+//         ?.map(({ name }) => name.toLowerCase())
+//         .includes(name.toLowerCase())
+//     ) {
+//       // toast(`${name} is already in contacts.`,
+//       //   {
+//       //   icon: '🤦🏻‍♂️',
+//       //   position: 'top-center',
+//       // });
+//       return;
+//     } else {
+//       dispatch(addContact(contact));
+//     }
+//     contactForm.reset();
+//   };
+
+//   return (
+//     <Container component="main" maxWidth="sm" theme={theme}>
+//       <Box
+//         component="form"
+//         onSubmit={handleSubmit}
+//         noValidate
+//         autoComplete="on"
+//         sx={{
+//           marginTop: 16,
+//           display: 'flex',
+//           flexDirection: 'column',
+//           alignItems: 'center',
+//         }}
+//       >
+//         {/* <ContactsForm onSubmit={handleSubmit}> */}
+//         <TextField
+//           required
+//           fullWidth
+//           type="text"
+//           label="Name"
+//           name="name"
+//           id={nameId}
+//         />
+//         <TextField
+//           required
+//           fullWidth
+//           type="text"
+//           label="Number"
+//           name="number"
+//           id={numberId}
+//         />
+//         <Button
+//           type="submit"
+//           // startIcon={<AddIcon />}
+//           variant="contained"
+//           color="primary"
+//           size="small"
+//           fullWidth
+//         >
+//           Add contact
+//         </Button>
+//         {/* </ContactsForm> */}
+//       </Box>
+//     </Container>
+//   );
+// };
+
+// export default ContactForm;
+
 import { useState } from 'react';
 import Notiflix from 'notiflix';
 import { useSelector, useDispatch } from 'react-redux';
@@ -47,23 +145,11 @@ export const ContactForm = () => {
     <ContactsForm onSubmit={handleSubmit}>
       <ContactsLabel>
         Name
-        <ContactsInput
-          type="text"
-          name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-        />
+        <ContactsInput type="text" name="name" required />
       </ContactsLabel>
       <ContactsLabel>
         Number
-        <ContactsInput
-          type="tel"
-          name="number"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="number number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
-        />
+        <ContactsInput type="tel" name="number" required />
       </ContactsLabel>
       <ContactsButton type="submit">Add contact</ContactsButton>
     </ContactsForm>
