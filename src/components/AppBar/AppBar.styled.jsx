107f85240625cec2c5f33e4457811346c622af95
Kateryna Ostrynska <kateriinag@gmail.com>
2023-01-30 01:08:46 +0200

    Update AppBar.styled.jsx

--- src/components/AppBar/AppBar.styled.jsx
+++ src/components/AppBar/AppBar.styled.jsx
@@ -1,9 +1,15 @@
 import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
 
 export const AppBarStyled = styled(AppBar)`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  ${'' /* padding: 16px; */}
+`;
+
+export const Link = styled(NavLink)`
+  display: flex;
+  text-decoration: none;
+  color: #fff5f0;
 `;
