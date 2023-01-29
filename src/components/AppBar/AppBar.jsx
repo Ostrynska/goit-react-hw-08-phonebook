d187ea13fce138644e3d219837f230d7078d9eeb
Kateryna Ostrynska <kateriinag@gmail.com>
2023-01-30 01:08:05 +0200

    Update AppBar.jsx

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,13 +1,14 @@
+import { useAuth } from 'hooks';
+
 import { Navigation } from '../Navigation/Navigation';
 import { UserMenu } from '../UserMenu/UserMenu';
 import { AuthNav } from '../AuthNav/AuthNav';
-import { useAuth } from 'hooks';
-import SummarizeRoundedIcon from '@mui/icons-material/SummarizeRounded';
 
+import SummarizeRoundedIcon from '@mui/icons-material/SummarizeRounded';
 import Container from '@mui/material/Container';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import { AppBarStyled } from './AppBar.styled';
+import { AppBarStyled, Link } from './AppBar.styled';
 
 export const AppBarNavigation = () => {
   const { isLoggedIn } = useAuth();
@@ -16,23 +17,20 @@ export const AppBarNavigation = () => {
     <AppBarStyled>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <SummarizeRoundedIcon sx={{ display: { md: 'flex' }, mr: 2 }} />
-          <Typography
-            variant="h6"
-            noWrap
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            MY CONTACTS
-          </Typography>
+          <Link to="/">
+            <SummarizeRoundedIcon sx={{ display: { md: 'flex' }, mr: 2 }} />
+            <Typography
+              variant="h6"
+              sx={{
+                mr: 6,
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+              }}
+            >
+              MY CONTACTS
+            </Typography>
+          </Link>
           <Navigation />
           {isLoggedIn ? <UserMenu /> : <AuthNav />}
         </Toolbar>
